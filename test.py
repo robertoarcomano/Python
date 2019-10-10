2df9812405e44f9a2d0d95be9af27a464133522a
Roberto Arcomano <bertolinux@gmail.com>
2019-10-10 23:28:56 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys;
+import math;
+
+def power(base,exp):
+    return math.pow(base,exp);
+
+def params():
+    s = "Params: \n"
+    for i,p in enumerate(sys.argv):
+        s += str(i) + " " + str(p) + "\n"
+    return s
+
+def file():
+    f = open("test.py","r")
+    return "File first line: " + f.readline() + "File chars: " + str(len(f.read()))
+
+def string(string):
+    s = "string: " + string + "\n"
+    for i,c in enumerate(string):
+        s += "[" + str(i) + "]: " + c + "\n"
+    s += "string[2:5]: " + string[2:6] + "\n"
+    s += "string[-2:len(string)]: " + string[-2:len(string)] + "\n"
+    return s
+
+print("2 power 3 = " + str(power(2,3)))
+print
+print params()
+print file()
+print
+print string(sys.argv[0])
