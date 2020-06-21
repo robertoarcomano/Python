4368c9303e33bdc04d0b438e540dcf1d0c97ea1e
Roberto Arcomano <bertolinux@gmail.com>
2020-06-21 17:33:18 +0100

    Added datatype.py

---
+++ datatype.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+n = 1
+s = "ok"
+
+print("str(n): " + str(n))
+print("str(type(n)): " + str(type(n)))
+print("s: " + s)
+print("str(type(s)): " + str(type(s)))
+print("s[0]: " + s[0])
+print("s[1]: " + s[1])
+print("s[-1]: " + s[-1])
+print("s[-2]: " + s[-2])
+print("s[0:1]: " + s[0:1])
+print("s[:1]: " + s[:1])
+print("s[1:]: " + s[1:])
+print("s[:]: " + s[:])
+print("len(s): " + str(len(s)))
+print
+l = list(s)
+print("l: " + str(l))
+print("str(type(l)): " + str(type(l)))
