645eaef2d5e8c6728ffc17740715be5701f328af
Roberto Arcomano <bertolinux@gmail.com>
2020-07-23 19:07:05 +0100

    Added new debug

--- threads.py
+++ threads.py
@@ -4,21 +4,29 @@ import threading
 global_var = 0
 num_thread = 8
 
+ret_list = list()
 
-def start(n):
+
+def start(n, ret):
     global global_var
     print(n, global_var)
     print(n, "++")
     global_var += n
     print(n, global_var)
+    ret.append({"self": 1, "n": n})
 
 
 threads = list()
 for i in range(0, num_thread):
-    threads.append(threading.Thread(target=start, args=(i,)))
+    threads.append(threading.Thread(target=start, args=(i, ret_list)))
 
 for i in range(0, num_thread):
     threads[i].start()
 
 for i in range(0, num_thread):
     threads[i].join()
+
+for i in range(0, num_thread):
+    print(threads[i])
+
+print(ret_list)
